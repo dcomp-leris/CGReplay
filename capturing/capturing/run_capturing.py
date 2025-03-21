cd235feff9cf17e229230ae335cb862cb9f7fd0f
Alireza <alireza_shirmarz@yahoo.com>
2025-03-21 13:01:29 -0300

    License was added!

--- capturing/capturing/run_capturing.py
+++ capturing/capturing/run_capturing.py
@@ -1,11 +1,28 @@
 '''
-Project: CGReplay
-Main Module: Capturing
-sub Module: Screen + Joystick
-Date: 2025-03-20 / time: 14:26
-Author(s): Alireza Shirmarz
-Location: Lerislab
+Copyright 2025 LERIS Lab 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 '''
+''' ************ Project Code Updates *************** 
+# Project: CGReplay
+# Main Module: Capturing
+# sub Module: Screen + Joystick
+# Date: 2025-03-20 / time: 14:26
+# Author(s): Alireza Shirmarz
+# Location: Leris lab
+''' ************************************************* 
+
 
 import subprocess
 import time
