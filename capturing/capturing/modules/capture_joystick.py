2aebe09ccb6703e657f28698de515cc8b3c21fdd
Alireza <alireza_shirmarz@yahoo.com>
2025-03-21 13:02:37 -0300

    license was added

--- capturing/capturing/modules/capture_joystick.py
+++ capturing/capturing/modules/capture_joystick.py
@@ -1,11 +1,26 @@
 '''
+Copyright 2025 LERIS Lab 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+''' ******************* Code Updates *******************
 Project: CGReplay
 Main Module: Capturing
 sub Module: Capturing the Joystick
 Date: 2025-03-10
 Author: Alireza Shirmarz
 Location: Lerislab
-'''
+''' ****************************************************
 import pygame
 import time
 import json
