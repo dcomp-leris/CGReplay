2659fbb941c9c8da2d56a8cc546f7e2ba2866067
alirezashirmarz <alireza_shirmarz@yahoo.com>
2025-07-14 13:36:01 -0300

    June 2025 Conference Setup

--- player/run_player.py
+++ player/run_player.py
@@ -11,8 +11,8 @@ import multiprocessing
 def run_player1():
     """Run the player Python script."""
     print("Player1 is running ....")
-    subprocess.run(["sudo","python3", "/home/leris/mygamer/tofino/player_tofino1.py"], check=True)
-
+    subprocess.run(["sudo","python3", "./cg_gamer_1.py"], check=True)
+    ## /home/alireza/CGReplay/CGReplay/player/cg_gamer_1.py
 '''
 def run_player2():
     """Run the player Python script."""
@@ -24,16 +24,16 @@ def run_player3():
     """Run the player Python script."""
     print("Player3 is running ....")
     subprocess.run(["sudo","python3", "/home/leris/mygamer/tofino/player_tofino3.py"], check=True)
-
+'''
 def run_tshark():
     """Run tshark command that requires sudo privileges."""
     cmd = ["tshark", "-i", "enp2s0np0", "-w", "./mypcap/my.pcap"]
     subprocess.run(cmd, check=True)
     print("Tshark is running ....")
 
-
+'''
 def run_kill_ports():
-    subprocess.run(["sudo","/home/leris/mygamer/tofino/port_clean1.sh"], check=True)
+    subprocess.run(["sudo","/port_clean1.sh"], check=True)
     print("killed the ports ***")
     time.sleep(1)
 
@@ -59,15 +59,15 @@ def run_delete_pcap():
 ''' 
 
 if __name__ == "__main__":
-    run_kill_ports()
-    run_delete_pcap()
-    run_delete_frames1()
-    run_delete_frames2()
-    run_delete_frames3()
+    #run_kill_ports()
+    #run_delete_pcap()
+    #run_delete_frames1()
+    #run_delete_frames2()
+    #run_delete_frames3()
 
     player1_process = multiprocessing.Process(target=run_player1)
-    player2_process = multiprocessing.Process(target=run_player2)
-    player3_process = multiprocessing.Process(target=run_player3)
+    #player2_process = multiprocessing.Process(target=run_player2)
+    #player3_process = multiprocessing.Process(target=run_player3)
     tshark_process = multiprocessing.Process(target=run_tshark)
     
     print('\n')
@@ -78,15 +78,15 @@ if __name__ == "__main__":
     
     # Start processes
     player1_process.start()
-    player2_process.start()
-    player3_process.start()
+    #player2_process.start()
+    #player3_process.start()
     tshark_process.start()
     
     #scream_process.start()
 
     # Wait for both to complete
     player1_process.join()
-    player2_process.join()
-    player3_process.join()
+    #player2_process.join()
+    #player3_process.join()
     tshark_process.join()
     #scream_process.join()
